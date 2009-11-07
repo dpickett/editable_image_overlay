00aa41cf4e330d7f4d953dea09f8160565b8f3a6
Dan Pickett <dpickett@enlightsolutions.com>
2009-11-07 09:12:11 -0500

    documentation and better defaults (don't show link by default)

--- javascripts/jquery.editable_image_overlay.js
+++ javascripts/jquery.editable_image_overlay.js
@@ -3,15 +3,21 @@
     var config = {};
 
     if (settings) $.extend(config, settings);
+    //add anchor properties
     $(this).children("a").addClass("avatar_overlay");
     $(this).children("a").addClass("hidden");
+    $(this).children("a").click(function(){
+      $(this).blur();
+    });
 
+    //bind hover functionality to anchors
     $(this).children("img").hover(function(){
       $(this).siblings("a").removeClass("hidden");
     },
     function(){
       $(this).siblings("a").addClass("hidden"); 
     });
+ 
     return this;
 
   };
